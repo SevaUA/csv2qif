09f5eee84b8a066abd2a905f087386dcb561980b
Sergey Pulyaev <serj1903@gmail.com>
2012-10-08 20:56:28 +0300

    beta 1

--- csv2qif.py
+++ csv2qif.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-15 -*-
+# coding: utf8
 '''
 CSV2QIF
 =======
@@ -11,43 +11,108 @@ import sys
 import csv
 from datetime import datetime
 
-def read_csv(statement_file):
+def read_csv(statement_file, output):
     ''' reads a csv statement file and returns the converted data
     '''
+    default_currency = 'UAH'
     csv_reader = csv.reader(statement_file, delimiter=';')
-    data = []
+    temp_data = []
     for row in csv_reader:
         try:
-            d_mov, d_val, desc, credit, debit = row[:5]
-            d_mov = datetime.strptime(d_mov, '%d-%m-%Y')
-            credit = credit.replace('.', '')
-            credit = credit.replace(',', '.')
-            if credit:
-                credit = float(credit)
+            date, account, category, total, currency, description, transfer = row[:7]
+            date = datetime.strptime(date, '%d.%m.%Y')
+
+            account = 'Активы:Текущие активы:' + account
+
+            total = total.replace('.', '')
+            total = total.replace(',', '.')
+            if total:
+                total = float(total)
             else:
-                credit = 0
-            debit = debit.replace('.', '')
-            debit = debit.replace(',', '.')
-            if debit:
-                debit = float(debit)
+                total = 0
+
+            category = category.replace('\\', ':')
+            if total >= 0:
+                category = 'Доходы:' + category
             else:
-                debit = 0
-            data.append([d_mov, desc, credit, debit, ])
+                category = 'Расходы:' + category
+
+            if currency != default_currency:
+                account = account + ' - ' + currency
+                category = category + ' - ' + currency
+
+            if transfer != '':
+                if temp_data == []:
+                    temp_data = [date, account, category, total, currency, description, transfer, ]
+                    continue
+            if temp_data == []:
+                trans = simple_trans(date, account, category, total, description)
+                write_to_qif(trans, output)
+            else:
+                currency_temp = temp_data[4]
+                account_temp = temp_data[1]
+                total_temp = temp_data[3]
+                if currency == currency_temp:
+                    trans = simple_trans(date, account, account_temp, total, description)
+                    write_to_qif(trans, output)
+                else:
+                    trans = multi_trans(date, account_temp, account, total_temp, total, description)
+                    write_to_qif(trans, output)
+                temp_data = []
+            #print csv_reader.line_num
         except Exception, e:
-            continue
-    return data
+            print csv_reader.line_num
+            print row
+            raise
 
-def convert_qif(data, outputfile):
-    for row in data:
-        outputfile.write('!Type:Bank\n')
-        outputfile.write('D%s\n' % (row[0].strftime("%d/%m/%Y"), ))
-        if row[2]:
-            outputfile.write('T-%s\n' % (str(row[2]), ))
-        else:
-            outputfile.write('T%s\n' % (str(row[3]), ))
-        outputfile.write('P%s\n' % (row[1], ))
-        outputfile.write('^\n')
+def simple_trans(date, account, category, total, description):
+    ret = '''!Account
+N{}
+TCash
+^
+!Type:Cash
+D{}
+T{}
+M{}
+L[{}]
+^
+'''.format(str(account),date.strftime("%d/%m/%Y"),str(total),str(description),str(category))
+    return ret
 
+def multi_trans(date, account_from, account_to, total_from, total_to, description):
+    ret = '''!Account
+N{0}
+TCash
+^
+!Type:Cash
+D{1}
+T{2}
+M{3}
+L[Собственные средства:Обмен]
+^
+!Account
+NСобственные средства:Обмен
+TCash
+^
+!Type:Cash
+D{1}
+T{5}
+M{3}
+L[{4}]
+^
+'''.format(
+    str(account_from),
+    date.strftime("%d/%m/%Y"),
+    str(total_from),
+    str(description),
+    str(account_to),
+    str(total_to)
+    )
+    return ret
+
+def write_to_qif(data, outputfile):
+    outputfile.write(data)
+    
 def main(statement_file):
     ''' driver function
     '''
@@ -65,8 +130,8 @@ def main(statement_file):
         print 'could not open output file: %s\n\t%s' % \
             (statement.name.replace('csv', 'qif'), str(error), )
 
-    data = read_csv(statement)
-    convert_qif(data, output)
+    read_csv(statement, output)
+
     print 'converted data saved to %s file' % \
             (statement.name.replace('csv', 'qif'), )
 
@@ -74,7 +139,6 @@ def main(statement_file):
     output.close()
 
 if __name__ == '__main__':
-
     if len(sys.argv) < 2:
         print './cvs2qif.py statement.csv'
         sys.exit(1)
